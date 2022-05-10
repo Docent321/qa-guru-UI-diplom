96fb91ed80b3a9ab41fce0b5dc50bca1452f1071
Alexandr Donskov <mrad@list.ru>
2022-05-11 00:27:36 +0300

    added testData

---
+++ src/test/java/testData/TestData.java
@@ -0,0 +1,10 @@
+package testData;
+
+public class TestData {
+    public static String
+            work = "ШТИЛЬ",
+            work1 = "15 лет",
+            author = "Дубинин",
+            value = "ШТИЛЬ",
+            author1 = "КИПЕЛОВ ВАЛЕРИЙ АЛЕКСАНДРОВИЧ";
+}
--- src/test/java/tests/RaoTest.java
+++ src/test/java/tests/RaoTest.java
@@ -2,22 +2,25 @@ package tests;
 
 import org.junit.jupiter.api.Test;
 import pages.RuRegistryPage;
+import testData.TestData;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static testData.TestData.*;
 
 public class RaoTest {
 
     RuRegistryPage ruRegistryPage = new RuRegistryPage();
-
+    TestData testData = new TestData();
+/*
     String
             work = "ШТИЛЬ",
             work1 = "15 лет",
             author = "Дубинин",
             value = "ШТИЛЬ",
             author1 = "КИПЕЛОВ ВАЛЕРИЙ АЛЕКСАНДРОВИЧ";
-
+*/
 
     @Test
     void homePageTest(){
