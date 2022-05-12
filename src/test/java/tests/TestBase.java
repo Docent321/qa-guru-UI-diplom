d1463e6b503cc307fbdfe247aa5ae26e707e4ee2
Alexandr Donskov <mrad@list.ru>
2022-05-13 00:19:21 +0300

    update TestBase

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,11 +1,9 @@
 package tests;
 
-import com.codeborne.selenide.Selenide;
+
 import com.codeborne.selenide.logevents.SelenideLogger;
-import config.Project;
 import helpers.AllureAttachments;
 import helpers.DriverConfig;
-import helpers.DriverUtils;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -31,20 +29,4 @@ public class TestBase {
         sleep(5000);
         closeWebDriver();
     }
-/*
-    @AfterEach
-    public void tearDown() {
-        String sessionId = DriverUtils.getSessionId();
-
-        AllureAttachments.addScreenshotAs("Last screenshot");
-        AllureAttachments.addPageSource();
-        AllureAttachments.addBrowserConsoleLogs();
-        Selenide.closeWebDriver();
-
-        if (Project.isVideoOn()) {
-            AllureAttachments.addVideo(sessionId);
-        }
-    }
-
- */
 }
